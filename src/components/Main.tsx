960cdd67a62e4cb4b1b5e9907a543934086f5c5c
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-24 19:07:14 -0400

    handle personal data changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,12 +2,35 @@ import React, { useState } from "react";
 import LeftWrapper from "./LeftWrapper";
 import NavBar from "./NavBar";
 import { Container } from "@chakra-ui/react";
+import { Remarkable } from "remarkable";
 
 const Main = () => {
+  const [cvData, setCvData] = useState({
+    personal: {},
+    summary: "",
+    experience: {},
+    education: {},
+    skills: {},
+    certifications: {},
+  });
+
+  const handlePersonalData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    setCvData((prev) => ({
+      ...prev,
+      personal: {
+        ...prev.personal,
+        [name]: value,
+      },
+    }));
+  };
+
   return (
     <div style={{ display: "flex" }}>
       <NavBar />
-      <LeftWrapper />
+      <LeftWrapper cv={cvData} onChangePersonal={handlePersonalData} />
     </div>
   );
 };
