193bba8bb552a7189f003a6869d670fc6d36dbd4
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-24 18:18:15 -0400

    navigation finished

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,14 +8,28 @@ import {
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { MoonIcon, SunIcon, QuestionOutlineIcon } from "@chakra-ui/icons";
+import PersonIcon from "@mui/icons-material/Person";
+import FeedIcon from "@mui/icons-material/Feed";
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from "@mui/icons-material/School";
+import BadgeIcon from "@mui/icons-material/Badge";
+import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
 
 const NavBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const bg = useColorModeValue("white", "#171717");
 
   const themeIcon = () => {
-    return bg == "white" ? <MoonIcon /> : <SunIcon />;
+    return bg == "white" ? (
+      <Text color="#718096" fontSize="2xl">
+        <MoonIcon />
+      </Text>
+    ) : (
+      <Text color="#718096" fontSize="2xl">
+        <SunIcon />
+      </Text>
+    );
   };
   return (
     <Container
@@ -27,11 +41,41 @@ const NavBar = () => {
       boxShadow="lg"
       alignItems="center"
       justifyItems="center"
+      justifyContent="space-between"
+      color="#718096"
+      p="2"
     >
       <Box onClick={toggleColorMode} cursor="pointer">
-        <Text fontSize="2xl">{themeIcon()}</Text>
+        {themeIcon()}
       </Box>
-      <Divider my="2" color="white" />
+      <Box
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        justifyItems="center"
+      >
+        <Link href="#profile">
+          <PersonIcon />
+        </Link>
+        <Link href="#summary">
+          <FeedIcon />
+        </Link>
+        <Link href="#work">
+          <WorkIcon />
+        </Link>
+        <Link href="#school">
+          <SchoolIcon />
+        </Link>
+        <Link>
+          <BadgeIcon />
+        </Link>
+        <Link>
+          <WorkspacePremiumIcon />
+        </Link>
+      </Box>
+      <Box>
+        <QuestionOutlineIcon />
+      </Box>
     </Container>
   );
 };
