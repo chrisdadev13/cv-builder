4e56d33c612cde3594cf1100ef84dae8315beed1
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-23 20:03:43 -0400

    left side and navbar building

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import LeftWrapper from "./LeftWrapper";
+import NavBar from "./NavBar";
+import { Container } from "@chakra-ui/react";
 
 const Main = () => {
   return (
-    <div>
+    <div style={{ display: "flex" }}>
+      <NavBar />
       <LeftWrapper />
     </div>
   );
