ec6aabf4c73c4c07a46478fbc01ac1ee115f6975
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-23 20:04:20 -0400

    dark mode and forms building

--- src/components/LeftWrapper.tsx
+++ src/components/LeftWrapper.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Tab from "./forms/Tab";
 import Summary from "./forms/Summary";
-import { Container } from "@chakra-ui/react";
+import { Container, useColorMode, useColorModeValue } from "@chakra-ui/react";
 
 const LeftWrapper = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue("white", "#171717");
   return (
-    <Container w="320px" px="10px">
+    <Container w="320px" px="10px" boxShadow="lg" bg={bg}>
       <Tab />
       <Summary />
     </Container>
