0489e012e24f546b1d690df08df7c98dcaac9aa5
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-25 14:04:28 -0400

    add experience field component and function

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,18 +4,26 @@ import NavBar from "./NavBar";
 import { Container } from "@chakra-ui/react";
 import { Remarkable } from "remarkable";
 
+interface StateProps {
+  personal: object;
+  experience: Array<string>;
+  education: Array<string>;
+  skills: object;
+  certificates: Array<string>;
+}
+
 const Main = () => {
-  const [cvData, setCvData] = useState({
+  const [cvData, setCvData] = useState<StateProps>({
     personal: {},
-    experience: {},
-    education: {},
+    experience: [],
+    education: [],
     skills: {},
-    certifications: {},
+    certificates: [],
   });
 
   const md = new Remarkable();
 
-  const getPersonalMarkup = (data) => {
+  const getPersonalMarkup = (data: string) => {
     return { __html: md.render(data) };
   };
 
@@ -23,7 +31,7 @@ const Main = () => {
     let name = event.target.name;
     let value = event.target.value;
 
-    setCvData((prev) => ({
+    setCvData((prev: any) => ({
       ...prev,
       personal: {
         ...prev.personal,
@@ -32,10 +40,35 @@ const Main = () => {
     }));
   };
 
+  const addExperienceField = () => {
+    if (cvData.experience.length < 5) {
+      setCvData((prev: any) => ({
+        ...prev,
+        experience: [
+          ...prev.experience,
+          {
+            id: prev.experience.length,
+            open: true,
+          },
+        ],
+      }));
+    }
+  };
+
+  const handleExperienceData = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: string
+  ) => {
+    let name = event.target.name;
+    let value = event.target.value;
+  };
+
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", height: "100vh" }}>
       <NavBar />
       <LeftWrapper cv={cvData} onChangePersonal={handlePersonalData} />
+      <button onClick={addExperienceField}>ADD button</button>
+      <button onClick={() => console.log(cvData)}>Test button</button>
     </div>
   );
 };
