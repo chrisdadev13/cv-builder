69d7c4a630b9c2acc56629b50c2fd6c16e354f5e
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-24 21:08:47 -0400

    minor changes, summary component

--- src/components/LeftWrapper.tsx
+++ src/components/LeftWrapper.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Tab from "./forms/Tab";
 import Summary from "./forms/Summary";
+import WorkExp from "./forms/WorkExp";
 import { Container, useColorMode, useColorModeValue } from "@chakra-ui/react";
 
 interface LeftTypes {
@@ -23,9 +24,17 @@ const LeftWrapper = ({
   const { colorMode, toggleColorMode } = useColorMode();
   const bg = useColorModeValue("white", "#171717");
   return (
-    <Container w="320px" h="full" px="10px" boxShadow="lg" bg={bg}>
+    <Container
+      w="320px"
+      h="full"
+      px="10px"
+      boxShadow="lg"
+      bg={bg}
+      overflowY="auto"
+    >
       <Tab personal={cv.personal} onChange={onChangePersonal} />
       <Summary personal={cv.personal} onChange={onChangePersonal} />
+      <WorkExp />
     </Container>
   );
 };
