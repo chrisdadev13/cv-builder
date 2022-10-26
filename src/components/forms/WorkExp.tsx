b4c76cc8a2e22b524cc0782fe922352503c7ac5f
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-25 20:42:40 -0400

    job preview container

--- src/components/forms/WorkExp.tsx
+++ src/components/forms/WorkExp.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, MouseEventHandler } from "react";
+import React, { useState, ChangeEventHandler, MouseEventHandler } from "react";
 import {
   Container,
   Box,
@@ -7,16 +7,23 @@ import {
   Input,
   Textarea,
   Button,
+  Spacer,
+  useColorModeValue,
 } from "@chakra-ui/react";
-import AddIcon from "@mui/icons-material/Add";
+import AddButton from "../utils/AddButton";
+import ExpModal from "../utils/ExpModal";
+import moment from "moment";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-interface ExperienceTYpes {
-  experience: Array<object>;
-  onChange: ChangeEventHandler<HTMLInputElement>;
-  onClick: MouseEventHandler<HTMLButtonElement>;
-}
+const formatDate = (dateValue: string) => {
+  console.log(moment(dateValue).format("LL"));
+  return moment(dateValue, "DD.MM.YYYY").format("LL");
+};
 
-const WorkExp = () => {
+const WorkExp = ({ experience, onClickAdd, onClickSave }) => {
+  const [currentModal, setCurrentModal] = useState("0");
+  const borderTheme = useColorModeValue("#EDF2F7", "#333");
+
   return (
     <Container w="full" color="#718096" fontSize="sm" textTransform="uppercase">
       <Heading
@@ -30,12 +37,35 @@ const WorkExp = () => {
       >
         Work Experience
       </Heading>
-      <Box>
-        <Button bg="white" border="solid" borderWidth="thin" ml="55%">
-          <AddIcon />
-          Add New
-        </Button>
-      </Box>
+      {experience.map((obj, index) => (
+        <Container
+          w="280px"
+          h="70px"
+          border="solid"
+          borderColor={borderTheme}
+          borderWidth="medium"
+          borderRadius="md"
+          py="7px"
+          px="5px"
+          my="5px"
+          textTransform="capitalize"
+          display="flex"
+          alignItems="center"
+        >
+          <Box>
+            <Heading fontSize="16px">{obj.companyName}</Heading>
+            <Text>
+              {formatDate(obj.startDate)} - {formatDate(obj.endDate)}
+            </Text>
+            <Text fontSize="10px">{obj.position}</Text>
+          </Box>
+          <Spacer />
+          <Box cursor="pointer">
+            <MoreVertIcon />
+          </Box>
+        </Container>
+      ))}
+      <AddButton onClick={onClickAdd} />
     </Container>
   );
 };
