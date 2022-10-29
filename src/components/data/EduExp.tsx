6e0f693606f096d8aef78d052986de6fbaf84a85
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-29 12:52:36 -0400

    education field and functions

--- src/components/data/EduExp.tsx
+++ src/components/data/EduExp.tsx
@@ -44,6 +44,8 @@ const EduExp = ({
   onClickDelete,
   onChange,
 }: eduTypes) => {
+  const borderTheme = useColorModeValue("#EDF2F7", "#333");
+
   return (
     <Container w="full" color="#718096" fontSize="sm" textTransform="uppercase">
       <Heading
@@ -57,7 +59,52 @@ const EduExp = ({
       >
         Education
       </Heading>
-      <AddButton />
+      {education.map((obj, index) => (
+        <Container
+          w="280px"
+          h="70px"
+          border="solid"
+          borderColor={borderTheme}
+          borderWidth="medium"
+          borderRadius="md"
+          py="7px"
+          px="5px"
+          my="5px"
+          textTransform="capitalize"
+          display="flex"
+          alignItems="center"
+          key={index}
+        >
+          <Box>
+            <Heading fontSize="16px">{obj.university}</Heading>
+            <Text>
+              {formatDate(obj.startDate) == ""
+                ? "Start date"
+                : formatDate(obj.startDate)}{" "}
+              -{" "}
+              {formatDate(obj.endDate) == ""
+                ? "End date"
+                : formatDate(obj.endDate)}
+            </Text>
+            <Text fontSize="10px">
+              {obj.degree}, {obj.studyField}
+            </Text>
+          </Box>
+          <Spacer />
+          <Box cursor="pointer" key={index} onClick={() => onClickEdit(index)}>
+            <EditIcon />
+          </Box>
+        </Container>
+      ))}
+      <EduModal
+        open={modalOpen}
+        data={education}
+        index={counter}
+        onChange={onChange}
+        onClickSave={onClickSave}
+        onClickDelete={onClickDelete}
+      />
+      <AddButton onClick={onClickAdd} />
     </Container>
   );
 };
