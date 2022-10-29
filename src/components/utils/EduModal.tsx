11fab3222253ff3c8674c1d9a89738c249aa7958
chrisdadev13 <chrisdadev13@gmail.com>
2022-10-29 12:51:53 -0400

    education field modal

--- src/components/utils/EduModal.tsx
+++ src/components/utils/EduModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEventHandler, ChangeEventHandler } from "react";
 import {
   Modal,
   ModalContent,
@@ -15,7 +15,16 @@ import {
 import { CloseIcon, DeleteIcon } from "@chakra-ui/icons";
 import InputMask from "react-input-mask";
 import educationTypes from "./interfaces/educationTypes";
-import { ModalType } from "./ExpModal";
+
+interface ModalType {
+  data: Array<any>;
+  index: number;
+  open: boolean;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  onClickSave: MouseEventHandler<HTMLButtonElement>;
+  onClickDelete: MouseEventHandler<HTMLButtonElement>;
+  onClickClose: MouseEventHandler<HTMLButtonElement>;
+}
 
 const EduModal = ({
   data,
@@ -35,10 +44,11 @@ const EduModal = ({
         textTransform="uppercase"
         fontSize="14px"
       >
-        <Box>
+        <Box display="flex">
           <Heading fontSize="18px" fontWeight="bold" textTransform="capitalize">
             Add Education
           </Heading>
+          <Spacer />
           <Box onClick={onClickSave} cursor="pointer">
             <CloseIcon />
           </Box>
@@ -50,8 +60,8 @@ const EduModal = ({
             w="100%"
             h="34px"
             variant="filled"
-            name="institution"
-            value={data[index].institution}
+            name="university"
+            value={data[index].university}
             onChange={(event) => onChange(event, index)}
           />
         </Box>
@@ -70,9 +80,98 @@ const EduModal = ({
         <Flex>
           <Box w="50%">
             <Text>Degree type</Text>
-            <Input type="text" w="180%" h="34px" 
+            <Input
+              type="text"
+              w="100%"
+              h="34px"
+              variant="filled"
+              name="degree"
+              value={data[index].degree}
+              onChange={(event) => onChange(event, index)}
+            />
+          </Box>
+          <Spacer mx="5px" />
+          <Box w="50%">
+            <Text>GPA</Text>
+            <Input
+              type="text"
+              w="100%"
+              h="34px"
+              variant="filled"
+              name="gpa"
+              value={data[index].gpa}
+              onChange={(event) => onChange(event, index)}
+            />
           </Box>
         </Flex>
+        <Flex my="20px">
+          <Box w="50%">
+            <Text>Start date</Text>
+            <InputMask
+              mask="99/99/9999"
+              maskChar=""
+              name="startDate"
+              value={data[index].startDate}
+              onChange={(event) => onChange(event, index)}
+            >
+              {() => (
+                <Input
+                  type="text"
+                  w="100%"
+                  h="34px"
+                  variant="filled"
+                  name="startDate"
+                  value={data[index].startDate}
+                />
+              )}
+            </InputMask>
+          </Box>
+          <Spacer mx="5px" />
+          <Box w="50%">
+            <Text>End date</Text>
+            <InputMask
+              mask="99/99/9999"
+              maskChar=""
+              name="endDate"
+              value={data[index].endDate}
+              onChange={(event) => onChange(event, index)}
+            >
+              {() => (
+                <Input
+                  type="text"
+                  w="100%"
+                  h="34px"
+                  variant="filled"
+                  name="endDate"
+                  value={data[index].endDate}
+                />
+              )}
+            </InputMask>
+          </Box>
+        </Flex>
+        <Box>
+          <Text>Summary</Text>
+          <Textarea
+            w="100%"
+            h="60px"
+            variant="filled"
+            resize="none"
+            name="summary"
+            value={data[index].summary}
+            onChange={(event) => onChange(event, index)}
+          />
+        </Box>
+        <Box display="flex" alignItems="center" mt="5">
+          <Button colorScheme="blue" onClick={onClickSave}>
+            Save
+          </Button>
+          {index !== 0 && (
+            <Button ml="2" colorScheme="red" onClick={onClickDelete}>
+              <DeleteIcon />
+              Delete
+            </Button>
+          )}
+        </Box>
       </ModalContent>
     </Modal>
   );
